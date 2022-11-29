6e5493ec95fc3185e6faaaebd117af27b505551d
BoccoSantiago <italobocco@gmail.com>
2022-11-29 16:05:59 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://bizmonthly.com/wp-content/uploads/2020/04/amazon-logo-black.png"
+        alt="Amazon-logo"
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+      <div className="header__optionLineTwo header__optionCart">
+        <ShoppingCartIcon />
+        <span className="header__optionLineTwo header__cartCount">0</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
