ae1d530fb0f6c081ce822664d2eeb92cee8c89b0
BoccoSantiago <italobocco@gmail.com>
2022-12-05 15:27:36 +0100

    fixed code smells

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const promise = loadStripe(
 );
 
 function App() {
-  const [{}, dispatch] = useStateValue();
+  const [dispatch] = useStateValue();
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
